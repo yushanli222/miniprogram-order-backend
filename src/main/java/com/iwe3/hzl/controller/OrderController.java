7a1cb6367faa385406d11b64cabf804832424a3b
yushanli222 <3243388281@qq.com>
2026-06-30 18:00:33 +0800

    优惠券的添加

--- src/main/java/com/iwe3/hzl/controller/OrderController.java
+++ src/main/java/com/iwe3/hzl/controller/OrderController.java
@@ -4,15 +4,15 @@ package com.iwe3.hzl.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping
-@RequestMapping("/wx/")
-public class OrderController {
-    @Autowired
-    grivate OrderService orderService;
+//@RequestMapping
+//@RequestMapping("/wx/")
+//public class OrderController {
+  //  @Autowired
+    //grivate OrderService orderService;
 
-    @RequestMapping("crateOrder")
-    public String crateOrder() {
+//    @RequestMapping("crateOrder")
+  //  public String crateOrder() {
 
 
-    }
-}
+    //}
+//}
--- src/main/java/com/iwe3/hzl/service/impl/OrderServiceImpl.java
+++ src/main/java/com/iwe3/hzl/service/impl/OrderServiceImpl.java
@@ -2,6 +2,6 @@ package com.iwe3.hzl.service.impl;
 
 import org.springframework.stereotype.Service;
 
-@Service
+@Serviceoo
 public class OrderServiceImpl {
 }
